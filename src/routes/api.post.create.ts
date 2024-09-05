4e91b874243f195287cd1d5b1625e94a9b1b76bf
Mateo Deget <degetmateo@gmail.com>
2024-09-05 00:21:40 -0300

    pre post replies

--- src/routes/api.post.create.ts
+++ src/routes/api.post.create.ts
@@ -6,33 +6,32 @@ module.exports = (server: Server) => {
         try {
             const content = req.body.post.content as string;
             const images = req.body.post.images as Array<string>;
+            const id_replied_post = req.body.post.id_replied_post || null;
+            const user = req.body.user;
             if ((content && content.trim().length < 1) || !content) return res.json({ ok:false, error: {message:"Debes escribir algo."} });
             if (content.length > 400) return res.json({ ok: false, error: { message: 'Has superado el limite de caracteres (400).'} });
             if (images && images.length > 4) return res.json({ ok:false, error: { message: "POST IMAGES: Hasta 4." } });
 
-            const user = req.body.user;
-            const queryUserCreator = await Postgres.query() `
-                SELECT * FROM
-                    member
-                WHERE
-                    username_member = ${user.username};
-            `;
-            
-            if (!queryUserCreator[0]) return res.json({ ok:false, error: { code: 'auth', message: 'Error de autentificacion.' } });
-    
-            await Postgres.query()`
-                SELECT insert_post (
-                    ${queryUserCreator[0].id_member},
-                    ${content},
-                    ${new Date().toISOString()},
-                    ${images || []}
-                );
-            `;
+            await Postgres.query().begin(async sql => {
+                await sql`SET TRANSACTION ISOLATION LEVEL READ COMMITTED;`;
+                const qCreator = await sql`
+                    SELECT * FROM member WHERE username_member = ${user.username};
+                `;
+                if (!qCreator[0]) throw new Error("Ha ocurrido un error.");
+                await sql`
+                    SELECT insert_post (
+                        ${qCreator[0].id_member},
+                        ${content},
+                        ${new Date().toISOString()},
+                        ${images || []}
+                    );
+                `;
+            });
     
             return res.json({ ok: true });
         } catch (error) {
             console.error(error);
-            return res.json({ ok: false, error: { message: "Ha ocurrido un error. Intentalo mas tarde." } });
+            return res.json({ ok: false, error: { message: "Ha ocurrido un error. Inténtalo mas tarde." } });
         }
     });
 }
